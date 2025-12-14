cf4331e0a93ba37e4972c6beaf210ac559fa274c
abdulgithub70 <saifiabduldelhi@gmail.com>
2025-12-14 23:26:17 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <>
+    <div>
+      <h1 className="text-3xl font-bold">Welcome to Stall Orders</h1>
+    </div>
+    </>
+  );
+}
