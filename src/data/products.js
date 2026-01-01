c02e63cccf2c8584aa5fee506783d5e4593e5bce
abdulgithub70 <saifiabduldelhi@gmail.com>
2026-01-01 16:06:02 +0530

    remove images

--- src/data/products.js
+++ src/data/products.js
@@ -1,15 +1,15 @@
 export const products = [
   {
     id: 1,
-    name: "Bisleri Water Bottle (1 Pack)",
-    price: 90,
+    name: "Bisleri Water Bottle",
+    price: 300,
     image: "/products/bisleri.jpg",
   },
   {
     id: 2,
-    name: "Bisleri Water Bottle (3 Pack)",
-    price: 270,
-    image: "/products/bisleri2.png",
+    name: "campa pack (20pcs)",
+    price: 50,
+    image: "/products/campa.png",
   },
   {
     id: 3,
@@ -19,81 +19,16 @@ export const products = [
   },
   {
     id: 4,
-    name: "campa pack (20pcs)",
-    price: 50,
-    image: "/products/campa.png",
-  },
-  {
-    id: 5,
-    name: "Burger",
-    price: 150,
-    image: "/products/burger1.png",
-  },
-  {
-    id: 6,
-    name: "Pizza",
-    price: 250,
-    image: "/products/pizza.png",
-  },
-  {
-    id: 7,
     name: "Coffe Machine Nescafe",
     price: 3000,
     description: "per day",
     image: "/products/Nescafe.jpg",
+    
   },
   {
-    id: 8,
-    name: "Pasta",
-    price: 200,
-    image: "/products/pizza.png",
-  },
-  {
-    id: 9,
-    name: "Cold Coffee",
-    price: 100,
-    image: "/products/cofee.png",
-  },
-  {
-    id: 10,
-    name: "Lemonade",
-    price: 80,
-    image: "/products/burger1.png",
-  },
-  {
-    id: 11,
-    name: "Iced Tea",
-    price: 90,
-    image: "/products/icetea.png",
-  },
-  {
-    id: 12,
+    id: 5,
     name: "LCD ",
     price: 130,
     image: "/products/tv.jpg",
-  },
-  {
-    id: 13,
-    name: "Vanilla Shake",
-    price: 130,
-    image: "/products/cofee.png",
-  },
-  {
-    id: 14,
-    name: "Strawberry Shake",
-    price: 130,
-    image: "/products/strawberryshake.png",
-  },
-  {
-    id: 15,
-    name: "Mango Shake",
-    price: 130,
-    image: "/products/mangoshake.png",
-  },
-  {
-    id: 16,
-    name: "Cold Drink (500ml)",
-    price: 60,
-    image: "/products/colddrink.png",
   }
 ];
